e72ba40fe5d1e02f3c32ec7935c2d7e36e0e0a50
POD666 <pod666@i.ua>
2014-09-09 11:52:11 +0300

    static

--- anymeter/settings.py
+++ anymeter/settings.py
@@ -81,11 +81,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 TEMPLATE_DIRS = [
     BASE_DIR + '/angular'
 ]
--- anymeter/urls.py
+++ anymeter/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from converter.api import v1_api
 from converter import views
@@ -8,4 +10,4 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1_api.urls)),
     url(r'^chain/(?P<src_id>\d+)/(?P<trg_id>\d+)/$', views.chain),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
